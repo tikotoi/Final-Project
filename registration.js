26699fcf9dbd8fcfe87e2f4ddcf5090950ea69c3
tikotoi <101997239+tikotoi@users.noreply.github.com>
2022-06-12 20:28:41 +0400

    final version

--- registration.js
+++ registration.js
@@ -104,3 +104,116 @@ for (let i = 0; i < accordion.length; i++) {
         this.classList.toggle('active');
     })
 }
+
+
+// Registration validation
+
+document.getElementById('registration').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    let errors = {};
+    let form = event.target;
+
+    let title = document.getElementById('title').value;
+
+    if (title.length < 4 || title == '') {
+        errors.title = 'Title can not be empty, numbers and must be more then 4 characters';
+    }
+
+
+    let description = document.getElementById('description').value;
+
+    if (description.length < 10) {
+        errors.description = 'Description must be more then 10 characters';
+    }
+
+    let password = document.getElementById('password1').value;
+    let password2 = document.getElementById('password2').value; 
+
+    if (password != password2 || password == '' || password.length < 8) {
+        errors.password = 'Password can not be empty and must be more then 8 characters';
+        errors.password2 = 'Passwords do not match';
+    }
+
+    let agree = document.getElementById('checkagree').checked;
+    if (!agree){
+        errors.agree = 'you must agree our terms and conditions';
+    }
+
+    let WebCategories = false;
+    form.querySelectorAll('[name = "categories"]').forEach(element => {
+        if (element.checked) {
+            WebCategories = true;
+        }
+    });
+
+    if (WebCategories == false){
+        errors.WebCategories = 'Please select your age';
+    }
+    
+    console.log(errors);
+
+    
+    form.querySelectorAll('.error-text').forEach(item =>{
+        item.innerHTML = '';
+    });
+
+    for (let item in errors){
+        console.log(item);
+        let errorSpan = document.getElementById('error_' + item);
+
+        if (errorSpan) {
+            errorSpan.textContent = errors[item];
+        }
+    }
+
+    if (Object.keys(errors).length == 0) {
+        form.submit();
+    }
+})
+
+
+let passwordShow = document.getElementById('password1');
+let passwordShow2 = document.getElementById('password2');
+let toggleIconEyereg = document.getElementById('toggleIconreg');
+
+showHidePassword = () => {
+    if(passwordShow.type == "password"){
+        passwordShow.setAttribute('type', 'text');
+        toggleIconEyereg.classList.add('fa-eye-slash');
+    }
+    else {
+        toggleIconEyereg.classList.remove('fa-eye-slash');
+        passwordShow.setAttribute('type', 'password');
+    }
+
+}
+toggleIconEyereg.addEventListener('click', showHidePassword);
+
+
+showHidePassword2 = () => {
+    if(passwordShow2.type == "password"){
+        passwordShow2.setAttribute('type', 'text');
+        toggleIconEyereg.classList.add('fa-eye-slash');
+    }
+    else {
+        toggleIconEyereg.classList.remove('fa-eye-slash');
+        passwordShow2.setAttribute('type', 'password');
+    }
+}
+
+toggleIconEyereg.addEventListener('click', showHidePassword2);
+
+function webValidation() {
+    let WebUrl = document.getElementById('regweb-url').value;
+    let WebUrlStrucutre = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+    let WebUrlSpan = document.getElementById('error_url');
+
+    if (WebUrl.match(WebUrlStrucutre)) {
+        WebUrlSpan.innerHTML = 'Your URL is valid';
+        WebUrlSpan.style.color = 'green';
+    } else {
+        WebUrlSpan.innerHTML = 'Your Email is invalid';
+        WebUrlSpan.style.color = 'red';
+    }
+}
